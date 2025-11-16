1ecce2c34483655f67ba9ac0f83ac2ab931486cd
empsloc <empsloc@gmail.com>
2025-11-16 20:05:26 +0530

    map changed

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,6 +3,7 @@ import { Input } from "@/components/ui/input";
 import { motion } from "framer-motion";
 import { FiMapPin, FiPhone, FiMail } from "react-icons/fi";
 import ContactForm from "../_components/ContactForm";
+import MapView from "../_components/Map";
 
 const ContactPage = () => {
   return (
@@ -62,15 +63,7 @@ const ContactPage = () => {
             </div>
 
             {/* Map */}
-            <div className="rounded-xl overflow-hidden aspect-[16/9] border border-gray-200">
-              <iframe
-                className="w-full h-full"
-                loading="lazy"
-                allowFullScreen
-                referrerPolicy="no-referrer-when-downgrade"
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3780.1210712519633!2d73.76632627609109!3d18.658562264942912!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2b93213084973%3A0x61cebb5002b5341e!2sgayatrree%20Veg%20Restaurant!5e0!3m2!1sen!2sin!4v1763233535928!5m2!1sen!2sin"
-              ></iframe>
-            </div>
+            <MapView/>
           </div>
         </div>
       </div>
